e6acba2666a16a93a95b82c774b9e4cd0fc77f1e
spikry <spikry@gmail.com>
2018-04-03 16:27:06 +0200

    Update hashgenerator.py

--- hashgenerator.py
+++ hashgenerator.py
@@ -1,12 +1,13 @@
 #! C:\python27
-import string, random,hashlib,binascii
+import string,random,hashlib,binascii
 special = ["-","_","!","@","*","$",".","?"]
 with open('words.txt') as words:
  for line in words:
      if len(line)>=8:
          special= random.choice(special)
+         number= random.randint(1,100)
          capitalized_string = line.capitalize()
-         full = capitalized_string+special
-         #print full
-         hash = hashlib.new('md4', full.encode('utf-16le')).digest()
-         print binascii.hexlify(hash)
+         full = capitalized_string+str(number)+special
+         print full
+         #hash = hashlib.new('md4', full.encode('utf-16le')).digest()
+         #print binascii.hexlify(hash)
