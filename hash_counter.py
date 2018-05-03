e1c761d9a6c678974ee1db29ca07c013995700a0
spikry <spikry@gmail.com>
2018-05-03 21:16:02 +0200

    Update hash_counter.py

--- hash_counter.py
+++ hash_counter.py
@@ -1,10 +1,13 @@
 #! C:\python27
 from collections import Counter
+import csv
 
 cnt = Counter ();
+write_file = "output.csv"
 
-for line in open('words.txt', 'r'):
- for word in line.split ():
-     cnt [word] += 1
-
-print (cnt)
+with open(write_file,"w") as output:
+    for line in open('E:\\Pentesting\\NTDS hashes\\combined_lm_ntlm.txt', 'r'):
+        for word in line.split ():
+            cnt [word] += 1
+    for k,v in cnt.most_common():
+        output.write( "{}, {}\n".format(k,v) )
