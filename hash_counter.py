b800bdad9bf49f744f7cd22f6586ccebaa08bc6f
ActivateDZA <spikry@gmail.com>
2018-06-10 17:52:20 +0200

    Update hash_counter.py
    hash counter for Rhodes University thesis

--- hash_counter.py
+++ hash_counter.py
@@ -2,12 +2,12 @@
 from collections import Counter
 import csv
 
-cnt = Counter ();
-write_file = "output.csv"
+cnt = Counter (); #counter
+write_file = "output.csv" # file name for output
 
-with open(write_file,"w") as output:
-    for line in open('combined_lm_ntlm.txt', 'r'):
-        for word in line.split ():
-            cnt [word] += 1
+with open(write_file,"w") as output: # reading file that contains the hashes
+    for line in open('combined_lm_ntlm.txt', 'r'): #file location that contains the hashes
+        for word in line.split (): #read the open file
+            cnt [word] += 1 # counter for hashes
     for k,v in cnt.most_common():
-        output.write( "{}, {}\n".format(k,v) )
+        output.write( "{}, {}\n".format(k,v) ) #output format that will be written to write_file
